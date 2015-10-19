18938d2f2faa886520293540571014b19076bfec
mam132 <mam132@duke.edu>
2015-10-18 21:57:21 -0400

    Making things run

--- fh/x86_64/fh.c
+++ fh/x86_64/fh.c
@@ -259,7 +259,7 @@ extern void _cvode_abstol( Symbol**, double*, int);
  	hoc_register_cvode(_mechtype, _ode_count, _ode_map, _ode_spec, _ode_matsol);
  	hoc_register_tolerance(_mechtype, _hoc_state_tol, &_atollist);
  	hoc_register_var(hoc_scdoub, hoc_vdoub, hoc_intfunc);
- 	ivoc_help("help ?1 fh /Users/matthewmccann/Documents/Code/CochlearNerveFHModel/fh/x86_64/fh.mod\n");
+ 	ivoc_help("help ?1 fh /Users/michellemueller/Documents/BME 504/Final Project/CochlearNerveFHModel/fh/x86_64/fh.mod\n");
  hoc_register_limits(_mechtype, _hoc_parm_limits);
  hoc_register_units(_mechtype, _hoc_parm_units);
  }
