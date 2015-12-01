44b696936a2c26b53a01b9cc69fe87c4ba42ac2b
mkm4884 <matthew.mccann13@gmail.com>
2015-11-30 21:11:54 -0500

    plots and stuff

--- plots.m
+++ plots.m
@@ -12,13 +12,18 @@ thresh_bend = [-10.05, -9.5, -8.6, -7.55, -6.5, -6.4, -6.25, -5.4, -4.95, -4.7, 
 
 figure()
 plot(node,thresh_bend)
-title('Current threshold vs. axon position')
+title('Current threshold vs. axon position for bent SGC')
 xlabel('Node Number')
 ylabel('Stimulation Threshold (nA)')
 
 
 %% Unmodified Fiber (without bend)
-thresh_no_bend = [];
+thresh_no_bend = [-10.25, -9.35, -8.55, -7.5, -6.45, -6.35, -6.25, -5.4, -5.4, -4.7, -4.7, -4.65, -4.6, -4.6, -4.6, -4.6, -4.6, -4.6, -4.6, -4.6, -4.6, -4.6, -4.65, -4.75, -4.95, -5.15, -4.95];
+figure()
+plot(node,thresh_no_bend)
+title('Current threshold vs. axon position for straight SGC')
+xlabel('Node Number')
+ylabel('Stimulation Threshold (nA)')
 
 %% Degraded Fiber
 
